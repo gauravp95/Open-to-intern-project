3132205f9500402167aa5776305139c67a9ec946
gaurav95 <gauravpatil4195@gmail.com>
2022-05-04 13:16:50 +0530

    created folders

---
+++ src/models/collegeModel.js
@@ -0,0 +1,6 @@
+{
+    "name" : "iith",
+    "fullName" : "Indian Institute of Technology, Hyderabad",
+    "logoLink" : "https://functionup.s3.ap-south-1.amazonaws.com/colleges/iith.png",
+    "isDeleted" : false
+} 
